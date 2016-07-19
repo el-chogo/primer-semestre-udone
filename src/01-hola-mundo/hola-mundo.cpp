c58e3aa9971efab634c2fff24232216bd826cb9d
Carlos Gottberg <42linoge@gmail.com>
2016-07-18 23:24:01 -0400

    Add CPP hello world example

---
+++ src/01-hola-mundo/hola-mundo.cpp
@@ -0,0 +1,10 @@
+#include <cstdio> // Libreria estandar de IO C++
+
+int main( int argc, char *argv[] ) {
+    /* 
+        Usamos el operador de resolucion :: para acceder a los elementos
+        dentro de la libreria estandar std
+    */
+    
+    std::cout << "Hola mundo!" << std::endl;
+}
