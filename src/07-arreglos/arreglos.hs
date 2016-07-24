5582bd50e27d22c709b9b76fe50946171309f594
Carlos Gottberg <42linoge@gmail.com>
2016-07-24 00:21:54 -0400

    Finish haskell array example

--- src/07-arreglos/arreglos.hs
+++ src/07-arreglos/arreglos.hs
@@ -1,6 +1,8 @@
-map' :: (a -> b) -> [a] -> [b]
-map' f [] = []
-map' f (x:xs) = f x : (map' f xs)
+import Data.Char
+
+mapear :: (a -> b) -> [a] -> [b]
+mapear f [] = []
+mapear f (x:xs) = f x : (mapear f xs)
 
 reducir :: b -> (a -> b -> b) -> [a] -> b
 reducir initial f [] = initial
@@ -19,10 +21,10 @@ todos f [] = True
 todos f (x:xs) = f x && alguno f xs
 
 alguno' :: (a -> Bool) -> [a] -> Bool
-alguno' f xs = reducir False (||) $ map' f xs
+alguno' f xs = reducir False (||) $ mapear f xs
 
 todos' :: (a -> Bool) -> [a] -> Bool
-todos' f xs = reducir True (&&) $ map' f xs
+todos' f xs = reducir True (&&) $ mapear f xs
 
 tomar :: Int -> [a] -> [a]
 tomar = tomar' []
@@ -44,3 +46,41 @@ tomarTantosCuando _ 0 _ = []
 tomarTantosCuando f n (x:xs)
     | f x = x : tomarTantosCuando f (n - 1) xs
     | otherwise = tomarTantosCuando f n xs
+
+divisor :: Int -> Int -> Bool
+divisor x y = (==) 0 $ mod y x
+
+verdadParaFunciones :: [(a -> Bool)] -> a -> Bool
+verdadParaFunciones fs x = todos' (\f -> f x) fs
+
+minusculas :: String -> String
+minusculas = mapear toLower
+
+palindromo :: String -> Bool
+palindromo a = (==) (minusculas a) (voltear $ minusculas a)
+
+data Cond a = a :? a
+
+infixl 1 :?
+infixl 0 ?
+
+(?) :: Bool -> Cond a -> a
+True ? (x :? _) = x
+False ? (_ :? x) = x
+
+main :: IO ()
+main = do
+    let predicados = [divisor 13, divisor 7]
+        numeros = tomarTantosCuando (verdadParaFunciones predicados) 20 [1..1000]
+
+    putStrLn "Numeros divisibles entre 13 y 7 del 1 al 1000"
+    putStrLn $ show $ numeros
+
+    putStrLn "Introduzca una palabra: "
+    palabra <- getLine
+    putStrLn $ (++) "Palabra volteada: " $ voltear palabra
+
+    putStrLn "Introduzca un palindromo: "
+    pal <- getLine
+    putStrLn $ "La palabra introducida " ++
+        ((palindromo pal) ? "es un" :? "no es un") ++ " palindromo"
