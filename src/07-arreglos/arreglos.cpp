b7e27ea8ad6d96e553c7276cef1562fcd03a9c44
Carlos Gottberg <42linoge@gmail.com>
2016-07-25 02:55:14 -0400

    Finish CPP array example, fix indentation

--- src/07-arreglos/arreglos.cpp
+++ src/07-arreglos/arreglos.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <functional>
+#include <cstring>
 
+using std::string;
 using std::vector;
 using std::cout;
 using std::cin;
@@ -10,101 +12,146 @@ using std::function;
 
 template<typename De, typename A>
 vector<A> mapear(function<A(De)> f, vector<De> xs) {
-  vector<A> ys;
-  for (auto x : xs) {
-    ys.push_back(f(x));
-  }
-  return ys;
+    vector<A> ys;
+    for (auto x : xs) {
+        ys.push_back(f(x));
+    }
+    return ys;
 }
 
 template<typename De, typename A>
 function<A(De)> siempre(const A x) {
-  return [x](De y) { return x; };
+    return [x](De y) { return x; };
 }
 
 template<typename T>
 T reducir(function<T(T,T)> f, vector<T> xs, T inicial) {
-  T resultado = inicial;
-  for (auto x: xs) {
-    resultado = f(resultado, x);
-  }
+    T resultado = inicial;
+    for (auto x: xs) {
+        resultado = f(resultado, x);
+    }
 
-  return resultado;
+    return resultado;
 }
 
 template<typename T>
 vector<T> voltear(vector<T> xs) {
-  vector<T> ys;
-  for (auto x : xs) {
-    ys.insert(ys.begin(), x);
-  }
-  return ys;
+    vector<T> ys;
+    for (auto x : xs) {
+        ys.insert(ys.begin(), x);
+    }
+    return ys;
 }
 
 template<typename T>
 vector<T> tomar(int n, vector<T> xs) {
-  vector<T> ys;
-  int contador = 0;
-  
-  for(auto x : xs) {
-    if (contador < n) {
-      ys.push_back(x);
-      contador++;
-    } else {
-      break;
-    }
-  }
+    vector<T> ys;
+    int contador = 0;
 
-  return ys;
+    for(auto x : xs) {
+        if (contador < n) {
+            ys.push_back(x);
+            contador++;
+        } else {
+            break;
+        }
+    }
+
+    return ys;
 }
 
 template <typename T>
 vector<T> imprimirVector(vector<T> xs) {
-  cout << "[";
-  for(auto x: xs) {
-    cout << " " << x;
-  }
-  cout << " ]" << endl;
-  return xs;
+    cout << "[";
+    for(auto x: xs) {
+        cout << " " << x;
+    }
+    cout << " ]" << endl;
+    return xs;
+}
+
+char minuscula(char a) {
+    return (a >= 65 && a <= 90) ? a - 32 : a;
+}
+
+vector<char> cadenaAVector(string cadena) {
+    vector<char> res;
+    for(auto x: cadena) {
+        res.push_back(x);
+    }
+    return res;
+}
+
+string vectorACadena(vector<char> cadena) {
+    string res;
+    for (auto x: cadena) {
+        res += x;
+    }
+    return res;
+}
+
+bool esPalindromo(string cadena) {
+    string cadenaMinuscula;
+
+    function<char(char)> minusculas = minuscula;
+
+    cadenaMinuscula = vectorACadena(mapear(minusculas, cadenaAVector(cadena)));
+
+    return cadenaMinuscula == vectorACadena(voltear(cadenaAVector(cadenaMinuscula)));
 }
 
 int sumar(int a, int b) {
-  return a + b;
+    return a + b;
 }
 
 
 int main(int argc, char *argv[]) {
-  function<bool(int)> siempreVerdad = siempre<int, bool>(false);
+    function<bool(int)> siempreVerdad = siempre<int, bool>(false);
 
-  vector<int> numeros = {1,2,3,4,5};
-  vector<bool> resultado;
-  int resultadoReducir;
-  function<int(int,int)> fSumar = sumar;
-  vector<int> volteados;
-  vector<int> resultadoTomar;
-  vector<int> resultadoTomarNada;
-  
-  resultado = mapear<int,bool>(siempreVerdad, numeros);
+    vector<int> numeros = {1,2,3,4,5};
+    vector<bool> resultado;
+    int resultadoReducir;
+    function<int(int,int)> fSumar = sumar;
+    vector<int> volteados;
+    vector<int> resultadoTomar;
+    vector<int> resultadoTomarNada;
+    string palabra;
+    bool palindromo;
 
-  cout << "Resultado de mapear [1,2,3,4,5] con f :: Int -> Bool" << endl;
-  imprimirVector<bool>(resultado);
-  
-  resultadoReducir = reducir<int>(fSumar, numeros, 0);
+    resultado = mapear<int,bool>(siempreVerdad, numeros);
 
-  cout << "Resultado de reducir [1,2,3,4,5] con f :: Int -> Int -> Int" << endl;
-  cout << resultadoReducir << endl;
+    cout << "Resultado de mapear [1,2,3,4,5] con f :: Int -> Bool" << endl;
+    imprimirVector<bool>(resultado);
 
-  cout << "Resultado de voltear [1,2,3,4,5]" << endl;
-  volteados = voltear<int>(numeros);
-  imprimirVector<int>(volteados);
+    resultadoReducir = reducir<int>(fSumar, numeros, 0);
 
-  cout << "Resultado de tomar 2 [1,2,3,4,5]" << endl;
-  resultadoTomar = tomar<int>(2, numeros);
-  imprimirVector<int>(resultadoTomar);
+    cout << "Resultado de reducir [1,2,3,4,5] con f :: Int -> Int -> Int" << endl;
+    cout << resultadoReducir << endl;
 
-  cout << "Resultado de tomar 0 [1,2,3,4,5]" << endl;
-  resultadoTomarNada = tomar<int>(0, numeros);
-  imprimirVector<int>(resultadoTomarNada);
-  
-  return 0;
+    cout << "Resultado de voltear [1,2,3,4,5]" << endl;
+    volteados = voltear<int>(numeros);
+    imprimirVector<int>(volteados);
+
+    cout << "Resultado de tomar 2 [1,2,3,4,5]" << endl;
+    resultadoTomar = tomar<int>(2, numeros);
+    imprimirVector<int>(resultadoTomar);
+
+    cout << "Resultado de tomar 0 [1,2,3,4,5]" << endl;
+    resultadoTomarNada = tomar<int>(0, numeros);
+    imprimirVector<int>(resultadoTomarNada);
+
+    cout << "Introduzca una palabra: ";
+    cin >> palabra;
+    cout << "Vectorizada: " << endl;
+    imprimirVector<char>(cadenaAVector(palabra));
+
+    palindromo = esPalindromo(palabra);
+
+    if (palindromo) {
+        cout << "La palabra introducida es un palindromo" << endl;
+    } else {
+        cout << "La palabra introducida no es un palindromo" << endl;
+    }
+
+    return 0;
 }
