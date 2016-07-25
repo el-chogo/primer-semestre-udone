2aab7f3f7c5d2b1d23824c8dae5c837e1fcadda4
Carlos Gottberg <42linoge@gmail.com>
2016-07-25 06:16:41 -0400

    Finish creating a generic container element

--- src/07-arreglos/arreglos.c
+++ src/07-arreglos/arreglos.c
@@ -1,33 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define ELEMENT(TYPE)                           \
+    struct TYPE ## ElementoContenedor
 
 #define MAKE_CONTAINER_ELEMENT(TYPE)            \
-    struct TYPE ## ElementoContenedor {         \
-        TYPE element;                           \
-        TYPE ## ElementoContenedor * next;      \
-    }
+    ELEMENT(TYPE) {                             \
+        TYPE elemento;                          \
+        ELEMENT(TYPE) * siguiente;              \
+    };
 
 #define MAKE_CONTAINER(TYPE)                    \
-    MAKE_CONTAINER_ELEMENT(TYPE);               \
+    MAKE_CONTAINER_ELEMENT(TYPE)                \
     struct TYPE ## Contenedor {                 \
-        TYPE ## ElementoContenedor * first;     \
+        ELEMENT(TYPE)  * primero;               \
         int containerSize;                      \
-    }
+    };
 
 #define CONTAINER(TYPE)                         \
     struct TYPE ## Contenedor
 
 #define ALLOC_CONTAINER(TYPE)                   \
-    malloc(sizeof(CONTAINER(TYPE)))
+    malloc(sizeof(CONTAINER(TYPE) *))
+
+#define ALLOC_ELEMENT(TYPE)                     \
+    malloc(sizeof(ELEMENT(TYPE) *))
 
 #define MAP_FUNCTION_NAME(TYPE_A, TYPE_B)       \
     mapear_ ## TYPE_A ## _ ## TYPE_B
 
 #define MAP_FUNCTION_SIG(TYPE_A, TYPE_B)        \
-    (CONTAINER(TYPE_A), TYPE_B (*f)(TYPE_A))
+    (CONTAINER(TYPE_A) xs, TYPE_B (*f)(TYPE_A))
 
 #define MAP(TYPE_A, TYPE_B)                                             \
-    CONTAINER(TYPE_B) * (MAP_FUNCTION_NAME(TYPE_A, TYPE_B) MAP_FUNCTION_SIG(TYPE_A, TYPE_B) { \
-            TYPE_B * nuevoContenedor = ALLOC_CONTAINER(TYPE_B);         \
-        }
+    CONTAINER(TYPE_B) * MAP_FUNCTION_NAME(TYPE_A, TYPE_B) MAP_FUNCTION_SIG(TYPE_A, TYPE_B) { \
+        CONTAINER(TYPE_B) * ys = ALLOC_CONTAINER(TYPE_B);               \
+        ys->primero = NULL;                                             \
+        ELEMENT(TYPE_A) * curX = xs.primero;                            \
+        ELEMENT(TYPE_B) ** curY;                                        \
+                                                                        \
+        if (curX == NULL) {                                             \
+            return NULL;                                                \
+        }                                                               \
+                                                                        \
+        curY = &(ys->primero);                                          \
+                                                                        \
+        while (curX != NULL) {                                          \
+            *curY = ALLOC_ELEMENT(TYPE_B);                              \
+            (*curY)->elemento = f(curX->elemento);                      \
+            (*curY)->siguiente = NULL;                                  \
+            curX = curX->siguiente;                                     \
+        }                                                               \
+        return ys;                                                      \
+    }
 
-MAP(int, char)
+
+MAKE_CONTAINER(int)
+MAKE_CONTAINER(char)
+MAP(int,char)
+
+char desdeA(int n) {
+    return 97 + n;
+}
+
+int main(int argc, char *argv[]) {
+    CONTAINER(int) listaNumeros;
+    listaNumeros.primero = ALLOC_ELEMENT(char);
+    listaNumeros.primero->elemento = 2;
+    listaNumeros.primero->siguiente = NULL;
+
+    CONTAINER(char) * listaLetras;
+    listaLetras = mapear_int_char(listaNumeros, &desdeA);
+
+    printf("%i\n", listaLetras->primero->elemento);
+
+    free(listaLetras->primero);
+    free(listaLetras);
+}
